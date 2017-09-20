458381dabaea4f1774501748dd6730dd4cdef100
estar73 <estarkov@gmail.com>
2017-09-20 21:55:14 +0500

    = regular update

--- Extract Text From Page Area/Program.cs
+++ Extract Text From Page Area/Program.cs
@@ -11,49 +11,44 @@
 
 
 using System;
-using System.IO;
-using System.Text;
 using Bytescout.PDFExtractor;
 using System.Drawing;
-using System.Diagnostics;
 
-namespace Example
+namespace ExtractTextFromPageArea
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             TextExtractor extractor = new TextExtractor("demo", "demo");
 
-            // load the document
-            extractor.LoadDocumentFromFile("../../sample2.pdf");
+            // Load document
+            extractor.LoadDocumentFromFile(@".\sample2.pdf");
 
-            // get page count
+            // Get page count
             int pageCount = extractor.GetPageCount();
-            int count = 0;
 
-            // iterate through pages
+            // Iterate through pages
             for (int i = 0; i < pageCount; i++)
             {
-
-                // define rectangle location to extract from
+                // Define rectangle location to extract from
                 RectangleF location = new RectangleF(0, 0, 200, 200);
                 
-                // set extraction area
+                // Set extraction area
                 extractor.SetExtractionArea(location);
 
-                // extract text bounded by the extraction area
-                string extractedString = extractor.GetTextFromPage(i);
+                // Extract text from the extraction area
+                string text = extractor.GetTextFromPage(i);
                 
-                Console.WriteLine("Extracted from page #" + i + ":\r\n" + extractedString);
+                Console.WriteLine("Extracted from page #" + i + ":");
+                Console.WriteLine();
+                Console.WriteLine(text);
 
-                // reset extraction area to full page (by default)
+                // Reset the extraction area
                 extractor.ResetExtractionArea();
 
-                Console.WriteLine("\r\n");
+                Console.WriteLine();
             }
-
 
 
             Console.WriteLine("Press any key to exit...");
