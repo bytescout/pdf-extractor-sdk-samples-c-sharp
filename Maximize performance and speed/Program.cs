d23ff183643f57378cb8c1068d1b037860cb6301
estar73 <estarkov@gmail.com>
2020-01-02 04:53:19 +0000

    = regular update

--- Maximize performance and speed/Program.cs
+++ Maximize performance and speed/Program.cs
@@ -14,7 +14,7 @@
 using System.Diagnostics;
 using Bytescout.PDFExtractor;
 
-// This example demonstrates the use of Optical Character Recognition (OCR) to extract text 
+// This example demonstrates the use of Optical Character Recognition (OCR) with "OCRMaximizeCPUUtilization" property to extract text 
 // from scanned PDF documents and raster images.
 
 // To make OCR work you should add the following references to your project:
@@ -39,43 +39,17 @@ namespace OCRExample
             extractor.OCRMode = OCRMode.Auto;
 
             // Set the location of OCR language data files
-            extractor.OCRLanguageDataFolder = @"c:\Program Files\Bytescout PDF Extractor SDK\ocrdata_best_fast\";
+            extractor.OCRLanguageDataFolder = @"c:\Program Files\Bytescout PDF Extractor SDK\ocrdata_best\";
 
             // Set OCR language
             extractor.OCRLanguage = "eng"; // "eng" for english, "deu" for German, "fra" for French, "spa" for Spanish etc - according to files in "ocrdata" folder
-            // Find more language files at https://github.com/bytescout/ocrdata/tree/master/ocrdata_fast
+            // Find more language files at https://github.com/bytescout/ocrdata
             
             // Set PDF document rendering resolution
             extractor.OCRResolution = 300;
 
-
-            // You can also apply various preprocessing filters
-            // to improve the recognition on low-quality scans.
-            
-            // Automatically deskew skewed scans
-            //extractor.OCRImagePreprocessingFilters.AddDeskew();
-
-            // Remove vertical or horizontal lines (sometimes helps to avoid OCR engine's page segmentation errors)
-            //extractor.OCRImagePreprocessingFilters.AddVerticalLinesRemover();
-            //extractor.OCRImagePreprocessingFilters.AddHorizontalLinesRemover();
-
-            // Repair broken letters
-            //extractor.OCRImagePreprocessingFilters.AddDilate();
-
-            // Remove noise
-            //extractor.OCRImagePreprocessingFilters.AddMedian();
-
-            // Apply Gamma Correction
-            //extractor.OCRImagePreprocessingFilters.AddGammaCorrection();
-
-            // Add Contrast
-            //extractor.OCRImagePreprocessingFilters.AddContrast(20);
-
-            
-            // (!) You can use new OCRAnalyser class to find an optimal set of image preprocessing 
-            // filters for your specific document.
-            // See "OCR Analyser" example.
-
+            // Enables max use of CPU and max use of multiple threads during OCR
+            extractor.OCRMaximizeCPUUtilization = true;
 
             // Save extracted text to file
             extractor.SaveTextToFile("output.txt");
